8bad8c5e00ea41092f2bff45ac1b1f5b30b97ce9
HarshCh0uhan <harshcoding2004@gmail.com>
2026-05-14 11:22:24 +0530

    feat: added room-route

---
+++ backend/src/routes/room.route.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+    createRoom,
+    getAllRooms,
+    getRoomById,
+    updateRoom,
+    deleteRoom,
+    assignStudent,
+    removeStudent,
+} from "../controllers/room.controller.js";
+import { authenticate, authorizeAdmin } from "../middleware/auth.middleware.js";
+
+const roomRouter = express.Router();
+
+roomRouter.use(authenticate);
+
+// Admin only
+roomRouter.post("/", authorizeAdmin, createRoom);
+roomRouter.put("/:id", authorizeAdmin, updateRoom);
+roomRouter.delete("/:id", authorizeAdmin, deleteRoom);
+roomRouter.post("/:id/assign", authorizeAdmin, assignStudent);
+roomRouter.post("/:id/remove", authorizeAdmin, removeStudent);
+
+// Admin + Student can view
+roomRouter.get("/", getAllRooms);
+roomRouter.get("/:id", getRoomById);
+
+export default roomRouter;
